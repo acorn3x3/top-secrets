9a9dbf7af4d6e2750f1c133f19f63aa80d07baf2
Andrew Cornejo <atcornejo3@gmail.com>
2022-11-25 15:27:22 -0800

    creates new user and tests that sampleUser has been sent correctly

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,42 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('users', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+
+const sampleUser = {
+  firstName: 'Test',
+  lastName: 'Testerson;',
+  email: 'test@test.com',
+  password: '12345',
+};
+
+
+
+
+
+
+  it('POST /api/v1/users creats a new user',  async () => {
+    const res = await (await request(app).post('api/v1/users')).setEncoding(sampleUser);
+    expect(res.status).toBe(200);
+    const {firstName, lastName, email } = sampleUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email
   });
+  });
+
+
+
+
   afterAll(() => {
     pool.end();
   });
