2b9be29880de0b3e7c54329f7c70024a5d448443
Andrew Cornejo <atcornejo3@gmail.com>
2022-12-04 12:58:54 -0800

    adjusted call/responses to be more accurate and uniform. Ran first test and passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,41 +1,34 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const request = require('supertest');
+//const request = require('supertest');
 const app = require('../lib/app');
-
-describe('users', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
-
+const request = require('supertest');
+//const UserService = require('../lib/services/UserService');
 
 const sampleUser = {
-  firstName: 'Test',
-  lastName: 'Testerson;',
+  first_name: 'Test',
+  last_name: 'Testerson',
   email: 'test@test.com',
   password: '12345',
 };
 
+describe('users routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
 
+  it('POST /api/v1/users creates a new user', async () => {
+    const resp = await request(app).post('/api/v1/users').send(sampleUser);
+    expect(resp.status).toBe(200);
+    const { first_name, last_name, email } = sampleUser;
 
-
-
-
-  it('POST /api/v1/users creats a new user',  async () => {
-    const res = await (await request(app).post('api/v1/users')).setEncoding(sampleUser);
-    expect(res.status).toBe(200);
-    const {firstName, lastName, email } = sampleUser;
-
-    expect(res.body).toEqual({
+    expect(resp.body).toEqual({
       id: expect.any(String),
-      firstName,
-      lastName,
-      email
+      first_name,
+      last_name,
+      email,
+    });
   });
-  });
-
-
-
 
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
-    id BIGINTT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    EMAIL TEXT NOT NULL,
+    EMAIL TEXT NOT NULL unique,
     password_hash TEXT NOT NULL
 );
